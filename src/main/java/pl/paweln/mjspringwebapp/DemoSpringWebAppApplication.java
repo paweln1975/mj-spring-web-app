188e60feaa59f9ad972f8d6d436df64fe0188395
Paweł <pawel.niedziela@gmail.com>
2020-12-30 16:51:58 +0100

    test

--- src/main/java/pl/paweln/mjspringwebapp/DemoSpringWebAppApplication.java
+++ src/main/java/pl/paweln/mjspringwebapp/DemoSpringWebAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoSpringWebAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringWebAppApplication.class, args);
 	}
-
 }
