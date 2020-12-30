422f7f34f188286c80075527d00966a4c2ba21a5
Paweł <pawel.niedziela@gmail.com>
2020-12-30 16:36:51 +0100

    initial commit

---
+++ src/main/java/pl/paweln/mjspringwebapp/DemoSpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package pl.paweln.mjspringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/paweln/mjspringwebapp/DemoSpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.paweln.mjspringwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
